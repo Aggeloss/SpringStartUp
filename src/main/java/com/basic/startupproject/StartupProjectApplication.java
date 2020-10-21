8b0177a163a288e4311c9b6f13271a859ddadb9e
Gikas Aggelos <angelosg@pcs.slg.gr>
2020-10-21 11:40:17 +0300

    Startup project compatible with WebLogic

---
+++ src/main/java/com/basic/startupproject/StartupProjectApplication.java
@@ -0,0 +1,21 @@
+package com.basic.startupproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.WebApplicationInitializer;
+
+@SpringBootApplication
+public class StartupProjectApplication extends SpringBootServletInitializer implements WebApplicationInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartupProjectApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(StartupProjectApplication.class);
+	}
+
+}
